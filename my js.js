79ff7451edac4c2edfb9dc738c06cd4c4dfd6ec5
o.akbari <o.akbari@ofogh.com>
2023-02-06 15:59:49 +0330

    third commit

--- my js.js
+++ my js.js
@@ -1,24 +1,40 @@
+
 function addTask(){ 
     const target = document.getElementById("task-container");
 
-    const div = document.createElement("div");
+    let div = document.createElement("div");
     div.setAttribute("class","item");
+    // div.setAttribute("id","item")
 
 
 
-    let div1 = document.createElement("div");
-    div1.setAttribute("class","div1");
+    let divMain = document.createElement("div");
+    divMain.setAttribute("class","divMain");
+    // divMain.setAttribute("id","div");
+
     var input =document.getElementById("text-input");
     var text = document.createTextNode(input.value);
 
-    div1.appendChild(text);
-    div.appendChild(div1);
+    divMain.appendChild(text);
+    div.appendChild(divMain);
 
 
 
 
-    var div2 =document.createElement("div");
-    div2.setAttribute("class","div2");
+
+
+
+
+
+
+
+
+    const divHelper = document.createElement("div");
+    divHelper.setAttribute("class","divHelper");
+
+    var favorite =document.createElement("div");
+    favorite.setAttribute("class","favorite");
+    favorite.setAttribute("id","div2");
 
     var heart = document.createElement("input");
     heart.setAttribute("type","checkbox");
@@ -29,19 +45,26 @@ function addTask(){
     var label1= document.createElement("label");
     label1.setAttribute("class","label1");
     label1.setAttribute("for","heart");
+    // label1.setAttribute("id","lable1");
 
     var stick1=document.createElement("img");
     stick1.setAttribute("src","emptyHeart.jpg");
     stick1.setAttribute("class","stick1");
+    // stick1.setAttribute("id","stick1");
     label1.appendChild(stick1);
-    div2.appendChild(heart);
-    div2.appendChild(label1);
-    div.appendChild(div2);
+    favorite.appendChild(heart);
+    favorite.appendChild(label1);
+    divHelper.appendChild(favorite);
 
 
 
-    var div3 = document.createElement("div");
-    div3.setAttribute("class","div3");
+
+
+
+
+
+    var history = document.createElement("div");
+    history.setAttribute("class","history");
     var d = new Date()
     var year =d.getFullYear();
     var month = d.getMonth() +1;
@@ -49,71 +72,87 @@ function addTask(){
     var dateString = year + "/" +  month + "/" + day ;
     var date = document.createTextNode(dateString);
     
-    div3.appendChild(date);
-    div.appendChild(div3);
+    history.appendChild(date);
+    divHelper.appendChild(history);
 
 
 
 
-    var div4= document.createElement("div");
-    div4.setAttribute("class","div4");
+
+
+
+
+
 
     var done = document.createElement("input");
     done.setAttribute("type","checkbox");
-    done.setAttribute("id","done");
+    // done.setAttribute("id","done");
     done.setAttribute("class","done");
     done.addEventListener("change",doneFunction);
-
-    var lable2 = document.createElement("label");
-    lable2.setAttribute("for","done");
-    lable2.setAttribute("class","lable2");
-
-    var stick2 = document.createElement("img");
-    stick2.setAttribute("src","greenTick.jpg");
-    stick2.setAttribute("class","stick2");
-
-    lable2.appendChild(stick2);
-    div4.appendChild(done);
-    div4.appendChild(lable2);
-
-    div.appendChild(div4);
+    divHelper.appendChild(done);
 
 
 
-    var div5 = document.createElement("div");
-    div5.setAttribute("class","div5");
-
-    var recyclebin = document.createElement("input");
-    recyclebin.setAttribute("type","checkbox");
-    recyclebin.setAttribute("id","recycle");
-    recyclebin.setAttribute("class","bin");
-    recyclebin.addEventListener("change",delFunction);
-
-    var lable3 = document.createElement("label");
-    lable3.setAttribute("for","recycle");
-    lable3.setAttribute("class","lable3");
-
-    var stick3 = document.createElement("img");
-    stick3.setAttribute("src","bin.png");
-    stick3.setAttribute("class","stick3");
-
-    lable3.appendChild(stick3);
-    div5.appendChild(recyclebin);
-    div5.appendChild(lable3);
-
-    div.appendChild(div5);
 
 
+
+
+
+
+
+
+    var remove = document.createElement("input");
+    remove.setAttribute("type","button");
+    remove.setAttribute("value","X");
+    // remove.setAttribute("id","recycle");
+    remove.setAttribute("class","remove");
+    remove.addEventListener("click",delFunction);
+
+    divHelper.appendChild(remove);
+    div.appendChild(divHelper);
 
     target.appendChild(div);
     console.log(Date());
+
+
+
+
+
+
+
+
+
+
+
+
+    document.getElementById("text-input").value=" ";
+
+
+
+    
 }
+
+
+
 
 function favFunction(){
 
-    alert("fav function");
+        // let img = this.parentNode.getElementsByClassName(label1).innerHtml;
+        // if(img === "emptyHeart.jpg" ){
 
-}
+        //     img= fullHeart.jpg ;
+        //     console.log("AHMAD");
+        //      alert("fav function");
+        // }
+        // else{
+           
+        //      console.log(img); 
+             
+        // }
+        alert("fav function222");
+
+    }
+
 
 
 function doneFunction (){
@@ -122,7 +161,14 @@ function doneFunction (){
 
 }
 
-function delFunction(){
-    alert("delete function");
 
+function delFunction(){
+ 
+    this.parentNode.parentNode.remove();
 }
+    
+
+
+
+
+
